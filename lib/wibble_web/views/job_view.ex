0a5a10fdffce807a6d7555ca55dd38081f0e7556
Justin Chang <changguangyu@yahoo.com>
2021-09-01 22:30:03 -0700

    Add jobs

---
+++ lib/wibble_web/views/job_view.ex
@@ -0,0 +1,19 @@
+defmodule WibbleWeb.JobView do
+  use WibbleWeb, :view
+  alias WibbleWeb.JobView
+
+  def render("index.json", %{jobs: jobs}) do
+    %{data: render_many(jobs, JobView, "job.json")}
+  end
+
+  def render("show.json", %{job: job}) do
+    %{data: render_one(job, JobView, "job.json")}
+  end
+
+  def render("job.json", %{job: job}) do
+    %{id: job.id,
+      company: job.company,
+      position: job.position,
+      status: job.status}
+  end
+end
